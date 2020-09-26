6076315b5fc294c19ac1d6e42acfef61d188eb8c
DavidReps <70821947+DavidReps@users.noreply.github.com>
2020-09-26 11:42:00 -0400

    Create ClientJoin.go

---
+++ ClientJoin.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide host:port.")
+		return
+	}
+
+	CONNECT := arguments[1]
+	conn, err := net.Dial("tcp", "localhost:"+CONNECT)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		//connect client to the currently existing server
+		go server.newClient(conn)
+
+	}
+}
