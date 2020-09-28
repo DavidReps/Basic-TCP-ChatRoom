d43410d55321c34469f2a23b59e3d71a773ee221
DavidReps <70821947+DavidReps@users.noreply.github.com>
2020-09-27 20:53:49 -0400

    Create command.go

---
+++ main/command.go
@@ -0,0 +1,20 @@
+package main
+
+type commandID int
+
+const (
+	CMD_Name commandID = iota
+	CMD_Join
+	CMD_Rooms
+	CMD_Msg
+	CMD_Private_Message
+	CMD_Exit
+	CMD_ListMembers
+)
+
+//each command needs to know the id, name of client calling it, and the actual string which may be required
+type command struct {
+	id     commandID
+	client *client
+	args   []string
+}
