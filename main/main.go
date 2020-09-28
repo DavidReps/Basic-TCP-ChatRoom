a8e093fe5cf1be750766dc3e9b455d2d1e92683a
DavidReps <70821947+DavidReps@users.noreply.github.com>
2020-09-28 19:46:05 -0400

    Update main.go

--- main/main.go
+++ main/main.go
@@ -1,26 +1,35 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 
-//we create a new server
+//we create a new server to listen and create the client structure when they join
 func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide server port number: \n ")
+	}
+	temp := bufio.NewReader(os.Stdin)
+	port, _ := temp.ReadString('\n')
 
+	//create new server
 	s := newServer()
-
 	//go routine to interpret commands from the client
 	go s.run()
 
-	listener, err := net.Listen("tcp", ":9000")
+	listener, err := net.Listen("tcp", ":" + port)
 	if err != nil {
 		log.Fatalf("unable to start server: %s", err.Error())
 	}
 
 	defer listener.Close()
-	log.Printf("server started on port :9000")
+	log.Printf("server started on port :" + port)
 
 	for {
 
@@ -31,8 +40,7 @@ func main() {
 			continue
 		}
 
-		//conn.Write([]byte(myTime))
-		//create new clients when they join
+		//create new clients as they join the server
 		go s.newClient(conn)
 
 	}
