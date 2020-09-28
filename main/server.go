95bc1d40786d7a8038a611a655868e045cf005c4
DavidReps <70821947+DavidReps@users.noreply.github.com>
2020-09-28 19:48:31 -0400

    Update server.go

--- main/server.go
+++ main/server.go
@@ -14,10 +14,6 @@ type server struct {
 
 }
 
-type Value struct{
-
-}
-
 func newServer() *server {
 	return &server{
 		rooms:    make(map[string]*room),
@@ -41,17 +37,14 @@ func (s *server) run() {
 		case CMD_Msg:
 			s.msg(cmd.client, cmd.args)
 
-			/*
 		case CMD_Private_Message:
-			s.FindClient(cmd.client, cmd.args[1])
+			s.Pmsg(cmd.client, cmd.args[1], cmd.args[2:])
 
-			s.PrivateMessage(cmd.client, cmd.args[2:])
-*/
 		case CMD_Rooms:
 			s.listRooms(cmd.client)
 
-//		case CMD_ListMembers:
-		//	s.listMembers(cmd.client)
+		case CMD_Back:
+			s.quitCurrentRoom(cmd.client)
 
 		case CMD_Exit:
 			s.quit(cmd.client)
@@ -59,42 +52,27 @@ func (s *server) run() {
 	}
 }
 
-/*
-//identify the client from command argument
-func (s *server) FindClient(c *client, name string) string{
-
-
-//find remote address of desired client
-	if val, ok := c.room.members[name.net.Addr]; ok {
-		return val.conn.RemoteAddr().String()
-	}
-
-	//c.DisplayMsg("Desired client is not in the room")
-	invalid := "Desired client is not in the room"
-	return 	invalid
-
-}
-*/
-
-//print on server side the remote address of new clients who connect
+//print on server side the remote address of new clients
 //read what the new client types
 func (s *server) newClient(conn net.Conn) {
 	log.Printf("new client has joined: %s", conn.RemoteAddr().String())
 
 	c := &client{
 		conn:     conn,
-		name:     "anonymous",
+		name:     "anonymous", //new clients are annon until changed
 		commands: s.commands,
 	}
-	fmt.Fprintf(conn, "connection made")
+	//send confirmation to client
+	fmt.Fprintf(conn, "confirm connection made \n ")
 
 	c.readInput()
 }
 
 //modify client structure's name field
+//todo only allow unique names to be chosen
 func (s *server) name(c *client, name string) {
 	c.name = name
-	c.msg(fmt.Sprintf("Hello %s", name))
+	c.msg(fmt.Sprintf("-> Hello %s", name))
 }
 
 //join a room is equal to adding client to the map of that room
@@ -119,7 +97,7 @@ func (s *server) join(c *client, roomName string) {
 	r.broadcast(c, fmt.Sprintf("%s has joined the chat", c.name))
 
 	//welcome the client to the room
-	c.DisplayMsg(fmt.Sprintf("-----Welcome to room: %s-----", roomName))
+	c.DisplayMsg(fmt.Sprintf("--------Welcome to room: %s--------", roomName))
 }
 
 
@@ -132,42 +110,25 @@ func (s *server) listRooms(c *client) {
 		rooms = append(rooms, name)
 	}
 
-	c.DisplayMsg(fmt.Sprintf("----------Available rooms:---------- \n%s ", strings.Join(rooms, "\n")))
+	c.DisplayMsg(fmt.Sprintf("------------Available rooms:------------ \n%s ", strings.Join(rooms, "\n")))
 }
-//
-////send a private message to a client within the room
-//func (s *server) PrivateMessage(c *client, args []string){
-//
-//	//check if name exists
-//	name := args[1]
-//	room := c.room
-//
-//	_, ok := room[name]
-//
-//	//send message to name of client
-//	if ok{
-//		message := strings.Join(args[1:len(args)], " ")
-//		c.msg(message)
-//	}
-//
-
-//}
 
 //standard room message broadcast
 //remove broadcast
 func (s *server) msg(c *client, args []string,) {
 	msg := strings.Join(args[1:len(args)], " ")
-	c.room.broadcast(c, c.name + ": "+msg)
+	c.room.broadcast(c, c.name + ": " + msg)
 }
 
 //leaving the room protocol
 func (s *server) quit(c *client) {
-	log.Printf("----------Client has left the chat:---------- \n%s", c.conn.RemoteAddr().String())
+	log.Printf("------------Client has left the chat:------------ \n%s", c.conn.RemoteAddr().String())
 
 	s.quitCurrentRoom(c)
 
 	c.msg("later fader")
 	c.conn.Close()
+
 }
 
 //exit from current room
@@ -182,32 +143,28 @@ func (s *server) quitCurrentRoom(c *client) {
 		delete(s.rooms[c.room.name].members, c.conn.RemoteAddr())
 
 		//notify current members of departure :(
-		oldRoom.broadcast(c, fmt.Sprintf("----------%s has left the room----------\n", c.name))
+		oldRoom.broadcast(c, fmt.Sprintf("------------%s has left the room------------\n", c.name))
 	}
 }
-//
-//func (s *server) listMembers(c *client){
-//	//initialize array of type value
-//	var names []reflect.Value
-//	room := c.room
-//	for members := range room.members{
-//
-//		names = append( ,names)
-//	}
-//
-//
-//}
 
-/*
-//list available rooms
-func (s *server) listRooms(c *client) {
-	var rooms []string
+func (s *server) Pmsg(c *client, recipient string, message []string){
 
-	//print all maps that exist within server
-	for name := range s.rooms {
-		rooms = append(rooms, name)
+
+	room := c.room
+	text := strings.Join(message, " ")
+
+	CompleteMessage := "From <" + c.name + "> (Privately) " + text
+
+//check if client is in the room
+	for _, v := range room.members {
+		if v.name == recipient {
+			//we have identified the client now send the message
+			v.msg(CompleteMessage)
+			return
+		}
 	}
+	//throw error if desired recipient is not present
+	c.err(fmt.Errorf("unknown client: %s", recipient))
+	return
+}
 
-	c.DisplayMsg(fmt.Sprintf("----------Available rooms:---------- \n%s ", strings.Join(rooms, "\n")))
-
- */
