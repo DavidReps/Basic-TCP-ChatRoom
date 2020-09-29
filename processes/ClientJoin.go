40a8c19d8c049975b4e6e09d4b1b52e98459edfb
DavidReps <70821947+DavidReps@users.noreply.github.com>
2020-09-28 21:58:33 -0400

    Update ClientJoin.go

--- processes/ClientJoin.go
+++ processes/ClientJoin.go
@@ -5,14 +5,42 @@ import (
 	"fmt"
 	"net"
 	"os"
-//	"strings"
+	// "strings"
 )
 
+func read(c net.Conn){
+
+	for {
+		msg := bufio.NewScanner(bufio.NewReader(c))
+		for msg.Scan(){
+			fmt.Println(msg.Text())
+		}
+	}
+}
+
+
+func write(c net.Conn){
+
+	for{
+
+		cmd,err := bufio.NewReader(os.Stdin).ReadString('\n')
+
+		if cmd == "EXIT"{
+			return
+		}
+		if err != nil {
+			fmt.Println("ERROR!")
+			return
+		}
+		c.Write([]byte(cmd))
+
+	}
+}
 func main(){
 
 	arguments := os.Args
 	if len(arguments) == 1 {
-		fmt.Println("Please provide host port number")
+		fmt.Println("Please provide host port number:")
 	}
 
 	temp := bufio.NewReader(os.Stdin)
@@ -21,21 +49,11 @@ func main(){
 	conn, err := net.Dial("tcp", "localhost:" + port)
 	if err != nil {
 		fmt.Println(err)
-		//return nil
 	}
-	for {
 
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(">> ")
+	defer conn.Close()
 
+	go read(conn)
+	write(conn)
 
-		text, _ := reader.ReadString('\n')
-
-		fmt.Fprintf(conn, text)
-
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Print(message)
-		//if strings.TrimSpace(string(text)) == "STOP" {
-		//	fmt.Println("TCP client exiting...")
-		}
-	}
+}
